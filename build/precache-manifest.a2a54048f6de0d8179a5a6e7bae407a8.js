0e6a69720102e0a36d3dca214134fc69bfb90b80
Srisai Nachuri <srisainachuri@berkeley.edu>
2020-07-29 20:00:23 -0700

    First commit

---
+++ build/precache-manifest.a2a54048f6de0d8179a5a6e7bae407a8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75aab3e6aa066f4dbf7431cc9800dc9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa819a595639778f69f5",
+    "url": "/static/css/main.bfd7a3a6.chunk.css"
+  },
+  {
+    "revision": "693e5ae8fb3dbdea6e51",
+    "url": "/static/js/2.893073f0.chunk.js"
+  },
+  {
+    "revision": "fa819a595639778f69f5",
+    "url": "/static/js/main.2b4c66a3.chunk.js"
+  },
+  {
+    "revision": "ac5ba503f516e69e7d8a",
+    "url": "/static/js/runtime-main.19a7b5ff.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0c0951c8538f58444ac4faa5a8b03b21",
+    "url": "/static/media/Srisai 4.0c0951c8.JPG"
+  },
+  {
+    "revision": "685cfc5dfd17578ac9bbc6296424db69",
+    "url": "/static/media/SrisaiNachuriResumeMay2020.685cfc5d.pdf"
+  },
+  {
+    "revision": "d19485b14229aa6beb1f17139709518f",
+    "url": "/static/media/Storefront.d19485b1.jpg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "db54c7efa5fe379d72ed15eb9da2c905",
+    "url": "/static/media/sms2.db54c7ef.png"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  }
+]);
